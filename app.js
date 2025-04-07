4d7dbbbd202fda286c1c8344ad767471434c955d
Ankur Sharma <ankur11murhu@gmail.com>
2025-04-07 12:37:52 +0530

    Always allow frontend

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    process.env.CLIENT_URL,"*"
   ],
   credentials: true,
 };
